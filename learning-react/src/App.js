9977a1c67bff45982e4a6cc12f5bfd86035ab3fa
Ibrahim Habbal <galactech.info@gmail.com>
2020-07-12 15:51:47 +1000

    Fresh React App Push

---
+++ learning-react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Test from './Components/Test'
+import CounterExample from './Components/CounterExample'
+
+function App() {
+  return (
+    <div>
+      {/*} <Test name="Yoooooo" /> */}
+      <CounterExample />
+    </div>
+  );
+}
+
+export default App;
---
+++ learning-react/src/Components/Test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Test extends React.Component{
+	render(){
+		return(
+				<h3>Hello {this.props.name}</h3>
+				
+			)
+	}
+}
+export default Test;
