5213a83ec772f880c247bd59a6f7e5102e3f1952
Ibrahim Habbal <galactech.info@gmail.com>
2020-07-27 11:57:14 +1000

    WHOLLLEE NEWWWW WORLLLLD

--- learning-react/src/Components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Footer(){
-    return (
-        <footer className="fixed text-white bg-actual-gray text-center text-xs p-3  border-t bottom-0 w-full">LiftMate</footer>
-    )
-}
-
-export default Footer;
